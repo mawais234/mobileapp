d8adea0e523fbca257b4dc0ab3d1bf6968fc8894
mawais234 <rehmatalimawais814@gmail.com>
2025-05-14 19:43:33 -0700

    textbox and image

--- main.dart
+++ main.dart
@@ -1,39 +1,77 @@
 import 'package:flutter/material.dart';
-import 'text_to_qr.dart';
-import 'qr_to_text.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: HomeScreen(),
-  ));
+  runApp(MyApp());
 }
 
-class HomeScreen extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Image Viewer',
+      debugShowCheckedModeBanner: false,
+      home: LocalImagesScreen(),
+      routes: {
+        '/global': (context) => GlobalImagesScreen(),
+      },
+    );
+  }
+}
+
+// Screen 1: Local Images (Vertical)
+class LocalImagesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('QR Code Utility')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              child: Text('Convert Text to QR Code'),
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (_) => TextToQrScreen()));
-              },
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              child: Text('Convert QR Code to Text'),
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (_) => QrToTextScreen()));
-              },
-            ),
-          ],
+      appBar: AppBar(
+        title: Text('Local Images (Vertical)'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.arrow_forward),
+            onPressed: () {
+              Navigator.pushNamed(context, '/global');
+            },
+          ),
+        ],
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(16),
+        children: [
+          Image.asset('assets/amir.jpeg', height: 200),
+          SizedBox(height: 16),
+          Image.asset('assets/download.jpeg', height: 200),
+          SizedBox(height: 16),
+          Image.asset('assets/SEO.png', height: 200),
+        ],
+      ),
+    );
+  }
+}
+
+// Screen 2: Global Images (Horizontal)
+class GlobalImagesScreen extends StatelessWidget {
+  final List<String> imageUrls = [
+    'https://picsum.photos/id/1018/300/200',
+    'https://picsum.photos/id/1015/300/200',
+    'https://picsum.photos/id/1020/300/200',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Global Images (Horizontal)'),
+      ),
+      body: Container(
+        height: 220,
+        padding: EdgeInsets.all(16),
+        child: ListView.separated(
+          scrollDirection: Axis.horizontal,
+          itemCount: imageUrls.length,
+          separatorBuilder: (context, index) => SizedBox(width: 16),
+          itemBuilder: (context, index) {
+            return Image.network(imageUrls[index]);
+          },
         ),
       ),
     );
